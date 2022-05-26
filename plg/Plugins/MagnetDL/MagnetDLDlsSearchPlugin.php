c57092271f9b6a31318e9bab7eccecf5a0750855
Adam Jakab <adam@jakab.pro>
2022-05-26 22:52:05 +0200

    adding 2 example plugins

---
+++ plg/Plugins/MagnetDL/MagnetDLDlsSearchPlugin.php
@@ -0,0 +1,54 @@
+<?php
+namespace Plugins\MagnetDL;
+/**
+ * The autoloader must be registered before any use statements.
+ * @see /console.php
+ */
+$pluginsBasePath = dirname(__DIR__);
+require_once $pluginsBasePath . '/SynoPluginLibrary/Autoloader.php';
+\Plugins\SynoPluginLibrary\Autoloader::register(["Plugins\\" => $pluginsBasePath]);
+/* ------------------------------------------------------------------------------ */
+
+use Plugins\SynoPluginLibrary\DlsPlugin;
+use Plugins\SynoPluginLibrary\DlsPluginInterface;
+
+/**
+ * MagnetDL DLS Search Plugin 
+ */
+class MagnetDLDlsSearchPlugin extends DlsPlugin implements DlsPluginInterface
+{
+    protected $debug = true;
+    protected $name = __CLASS__;
+    /**
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->log("Constructed.");
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see DlsPluginInterface::prepare()
+     */
+    public function prepare($curl, $query)
+    {
+        parent::prepare($curl, $query);
+        $searchurl = "https://example.com/?s=%s";
+        $searchurl = sprintf($searchurl, urlencode($query));
+        curl_setopt($curl, CURLOPT_URL, $searchurl);
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see DlsPluginInterface::parse()
+     */
+    public function parse($plugin, $searchPageHtml)
+    {
+        parent::parse($plugin, $searchPageHtml);
+        $this->log("Parsing...");
+    }
+}
+
